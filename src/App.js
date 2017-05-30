46caaf7c369d2441b0743ae98ec5d91f6109a65a
marleysidapple <bsiddhartha25@gmail.com>
2017-05-30 18:17:02 +0800

    first push

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+
+	constructor(props){
+		super(props);
+	}
+
+
+	render(){
+		return (
+			<View>
+				<Text>This is app</Text>
+			</View>
+		);	
+	}
+}
+
+export default App;
